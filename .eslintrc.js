1e063196d60365382ee09eacee9920de7a18ed9f
Socheat <socheatsok78@gmail.com>
2017-11-29 15:44:37 +0700

    Install linting system ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "es6": true,
+    "browser": true,
+    "commonjs": true,
+    "jquery": true,
+  },
+  "globals": {},
+  "plugins": [],
+  "rules": {
+    "no-console": "off",
+    "global-require": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "off"
+  }
+};
