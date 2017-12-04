5472d8ab17f4bb6dba65024e16fdb6991b57d759
Socheat <socheatsok78@gmail.com>
2017-12-04 10:55:24 +0700

    Update d2r.js sdk, validate incoming url
    Return an alert if url is invalid

--- src/sdk/d2r.js
+++ src/sdk/d2r.js
@@ -5,12 +5,39 @@ import { Base64 } from 'js-base64';
  * http://s.miwifi.com/d2r/
  * http://d.miwifi.com/d2r/?url=
  * @param {string} url Base64 encoded download address, support (http, ftp, torrent, eMule, Thunder)
- * @param {string} name
- * @param {string} src
+ * @returns {string}
  */
-export default function download(url, name, src) {
-  const server = 'http://d.miwifi.com/d2r/?url=';
-  const downloadLink = Base64.encodeURI(url);
-  const source = `&src=${src}`;
-  return `${server}${downloadLink}${source}`;
+export default function download(url) {
+  const server = 'https://d.miwifi.com/d2r/?url=';
+  
+  /**
+   * Validating URL
+   */
+  try {
+    let downloadLink = new URL(url);
+    let filename = `&name=${getFilename(downloadLink)}`;
+    let source = `&src=${downloadLink.hostname}`;
+    let hash = Base64.encodeURI(url);
+
+    /**
+     * Sending to Xiaomi Server
+     */
+    return `${server}${hash}${filename}${source}`;
+  } catch (e) {
+    /**
+     * Report invalid url to the user
+     */
+    alert(e);
+    return false;
+  }
+
+}
+
+/**
+ * Get filename based on url
+ * @param {URL} url
+ */
+function getFilename(url) {
+  let file = url.pathname.split('/');
+  return `remote-${file[file.length - 1]}`;
 }
