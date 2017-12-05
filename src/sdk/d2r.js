ce183842f368e7bb0368c7a16e031afecce7e418
Socheat <socheatsok78@gmail.com>
2017-12-05 09:56:21 +0700

    d2r.js add file extension detection

--- src/sdk/d2r.js
+++ src/sdk/d2r.js
@@ -1,4 +1,5 @@
 import { Base64 } from 'js-base64';
+import forEach from 'lodash/forEach';
 
 /**
  * File Download Protocol for Mi Wifi
@@ -36,8 +37,27 @@ export default function download(url) {
 /**
  * Get filename based on url
  * @param {URL} url
+ * @returns {string}
  */
 function getFilename(url) {
   let file = url.pathname.split('/');
-  return `remote-${file[file.length - 1]}`;
+  return `remote-${file[file.length - 1]}.${getExtension(url)}`;
+}
+
+/**
+ * Try to guest file extension based on url
+ * @param {URL} url
+ * @returns {string}
+ */
+function getExtension(url) {
+  let pathname = url.pathname;
+  let defaultExtension = 'ext';
+  const pattern = /(?:\.(\w+\D))$/i;
+
+  let extension = defaultExtension;
+  if (pattern.test(pathname)) {
+    extension = pathname.match(pattern)[1];
+  }
+
+  return extension;
 }
